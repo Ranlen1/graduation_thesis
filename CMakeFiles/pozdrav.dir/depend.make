ef4a78ecfaca0f8f181d0ab2aaeccf26e5405c99
Ranlen <rene.cakan@gmail.com>
2025-06-23 18:47:16 +0200

    cmake setup

---
+++ CMakeFiles/pozdrav.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pozdrav.
+# This may be replaced when dependencies are built.
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int x = 20;
+    std::cout << "Hello, world" << ' ' << x << '\n';
+}
